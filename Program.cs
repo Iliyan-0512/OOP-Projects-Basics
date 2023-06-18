855a775a655d22fbe824c0c3da3e184af7d34aed
iliyan <imanov05@abv.bg>
2023-06-18 18:06:51 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            int sum = 0;
+            do
+            {
+                number = int.Parse(Console.ReadLine());
+                sum += number;
+
+            } while (number!=0);
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
